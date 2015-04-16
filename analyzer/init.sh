f3c57022e9f47b0a1e46937332a6bc7e1b8b3933
Sébastien <sebastien.wertz@uclouvain.be>
2015-04-16 12:04:53 +0200

    Added init.sh for environment settings

---
+++ analyzer/init.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+module load python/python27_sl6_gcc49
+export PYTHONPATH="/home/fynu/sbrochet/.local/lib/python2.7/site-packages:$PYTHONPATH"
+module load gcc/gcc-4.9.1-sl6_amd64
+module load root/6.02.05-sl6_gcc49

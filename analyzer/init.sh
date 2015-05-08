4bc6b22f52bef4971a95fe310a56551674b1c93d
Sébastien Brochet <sebastien.brochet@cern.ch>
2015-05-08 13:50:20 +0200

    init.sh now works on lxplus too

--- analyzer/init.sh
+++ analyzer/init.sh
@@ -1,4 +1,21 @@
-#!/bin/bash
+# No shebang here as this script must be sourced and not executed
 
-module load pheno/pheno_gcc49 # loads python2.7, root 6.02, boost, gcc 4.9.2
-export PYTHONPATH="/home/fynu/sbrochet/.local/lib/python2.7/site-packages:$PYTHONPATH"
+if [[ $HOST == lxplus* ]]; then
+
+    source /cvmfs/cms.cern.ch/slc6_amd64_gcc491/external/gcc/4.9.1-cms/etc/profile.d/init.sh
+    source /cvmfs/cms.cern.ch/slc6_amd64_gcc491/external/boost/1.57.0-cms/etc/profile.d/init.sh
+    source /cvmfs/cms.cern.ch/slc6_amd64_gcc491/external/python/2.7.6-cms/etc/profile.d/init.sh
+    source /cvmfs/cms.cern.ch/slc6_amd64_gcc491/external/libjpg/8b-cms/etc/profile.d/init.sh
+    source /cvmfs/cms.cern.ch/slc6_amd64_gcc491/external/libpng/1.6.16/etc/profile.d/init.sh
+    source /cvmfs/cms.cern.ch/slc6_amd64_gcc491/external/xz/5.0.3__5.1.2alpha-cms/etc/profile.d/init.sh
+    source /cvmfs/cms.cern.ch/slc6_amd64_gcc491/lcg/root/6.02.00-cms4/etc/profile.d/init.sh
+    source /cvmfs/cms.cern.ch/slc6_amd64_gcc491/external/git/1.8.3.1-cms2/etc/profile.d/init.sh
+    export PYTHONPATH="/afs/cern.ch/user/s/sbrochet/.local/lib/python2.7/site-packages:$PYTHONPATH"
+
+else
+
+    module load pheno/pheno_gcc49 # loads python2.7, root 6.02, boost, gcc 4.9.2
+    export PYTHONPATH="/home/fynu/sbrochet/.local/lib/python2.7/site-packages:$PYTHONPATH"
+
+fi
+

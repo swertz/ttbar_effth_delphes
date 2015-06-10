4a695a361d0c15d7944bc096859fd458526dc078
Sébastien <sebastien.wertz@uclouvain.be>
2015-06-10 11:49:41 +0200

    Added options for running driver

--- analyzer/init.sh
+++ analyzer/init.sh
@@ -14,7 +14,9 @@ if [[ $HOSTNAME == lxplus* ]]; then
 
 else
 
-    module load pheno/pheno_gcc49 # loads python2.7, root 6.02, boost, gcc 4.9.2
+    module load python/python27_sl6_gcc49
+    module load gcc/gcc-4.9.1-sl6_amd64
+    module load root/6.02.05-sl6_gcc49
     export PYTHONPATH="/home/fynu/sbrochet/.local/lib/python2.7/site-packages:$PYTHONPATH"
 
 fi
